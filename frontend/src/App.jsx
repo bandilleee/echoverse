96dd232e86fa6b30b085df237bb89dd13e151dbf
Bandile <bandilesimdumise@gmail.com>
2025-08-21 10:18:11 +0200

    Initial commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,7 +7,7 @@ import Dashboard from './pages/admin/Dashboard'
 import AddBlog from './pages/admin/AddBlog'
 import ListBlog from './pages/admin/ListBlog'
 import Comments from './pages/admin/Comments'
-import Login from './components/admin/login'
+import Login from './components/admin/login.jsx'
 import 'quill/dist/quill.snow.css'
 
 const App = () => {
